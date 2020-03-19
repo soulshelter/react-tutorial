dc68de57b0b40e94997aa05311b44bd166f5ced9
auroville <auroville.pms@gmail.com>
2020-03-19 14:56:43 +0900

    Change : 시간여행 구현하기
    history를 이용하여 게임 기록을 저장하고 이전 진행으로 이동 할수 있게 수정

--- src/index.js
+++ src/index.js
@@ -56,13 +56,14 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
 
     handleClick(i) {
         // slice를 이용하여 불변성 구성
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -73,14 +74,33 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         let status;
         if (winner) {
@@ -99,7 +119,7 @@ class Game extends React.Component {
 
                 <div className="game-info">
                     <div>{status}</div>
-                    <div>{/* TODO */}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
